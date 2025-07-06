58a8897896ff433f567c4d4f651484f167e35184
jiwonpg98 <jiwonpg98@gmail.com>
2025-07-06 17:13:56 +0900

    main: 다른거 제거

--- src/main/kotlin/redis/practice/scheduler/config/ScheduleConfig.kt
+++
@@ -1,8 +0,0 @@
-package redis.practice.scheduler.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableScheduling
-
-@Configuration
-@EnableScheduling
-class ScheduleConfig
